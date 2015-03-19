7c25d7cbf943bd02eac310a8b1c8d3ffa1fe4d21
Spideynn <spideynn@gmail.com>
2015-03-19 19:09:12 -0400

    Fixed some bugs, got password encryption working.

--- app.py
+++ app.py
@@ -2,42 +2,74 @@
 import os
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, render_template, flash
+import getpass
+import base64
+import easygui
 
 app = Flask(__name__)
-
+### CONFIGURATION SECTION ###
 ### EDIT BELOW HERE ###
+SECRET_KEY='8v0w9e8RNWEVRW90e8rnvWER9837R' # Match this with the secret key below.
 app.config.update(dict(
     DATABASE=os.path.join('data/', 'blazegoat.db'),
     DEBUG=True,
-    SECRET_KEY='8v0w9e8RNWE*VRW90e8rnvWER&9837R'
+    SECRET_KEY='8v0w9e8RNWEVRW90e8rnvWER9837R'
 ))
 ### EDIT ABOVE HERE ###
+
+### ENCRYPT PASSWORDS ###
+def encrypt(key, msg):
+    encryped = []
+    for i, c in enumerate(msg):
+        key_c = ord(key[i % len(key)])
+        msg_c = ord(c)
+        encryped.append(chr((msg_c + key_c) % 127))
+    return ''.join(encryped)
+
+def decrypt(key, encryped):
+    msg = []
+    for i, c in enumerate(encryped):
+        key_c = ord(key[i % len(key)])
+        enc_c = ord(c)
+        msg.append(chr((enc_c - key_c) % 127))
+    return ''.join(msg)
+### END ENCRYPTED PASSWORDS ###
 
 def connect_db(): # Connect to the database specified in the config array.
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
 
-
 def init_db(): # Initialize database if it doesn't exist.
     if os.path.exists("data/blazegoat.db"):
-        print("Database exists, assuming schema does too.")
+        print("Database exists, assuming schema does too.") # Assume that the schema is valid, and continue.
         db = sqlite3.connect('data/blazegoat.db')
         db.close()
     else:
         print("Database does not exist, creating.")
+        username = easygui.enterbox('Please enter the admin account username.')
+        password = encrypt(SECRET_KEY, easygui.passwordbox('Please enter a password that is longer than eight characters. (hidden)'))
+        while(len(password) < 8):
+            password = encrypt(SECRET_KEY, easygui.passwordbox('Password is less than eight characters\nPlease enter a password that is longer than eight characters. (hidden)'))
+        email = easygui.enterbox('Please enter your email address.')
         db = sqlite3.connect('data/blazegoat.db')
+        
         cursor = db.cursor() # Create the users table.
+        cursor.execute('PRAGMA secure_delete = "1"') # Undeletable users and server data, preventing stolen data.
+        
         cursor.execute('''
         CREATE TABLE users(id INTEGER PRIMARY KEY, username TEXT UNIQUE,
-        email TEXT UNIQUE, password TEXT, rank INT)
+        email TEXT UNIQUE, password TEXT, rank INT, tempPass INTEGER)
         ''') # Create the servers database.
         cursor.execute('''
         CREATE TABLE servers(id INTEGER PRIMARY KEY, sid INTEGER UNIQUE,
         owner TEXT, jar TEXT)
         ''')
         db.commit()
-        db.close() 
+        cursor.execute('INSERT INTO users (username, email, password, rank, tempPass) VALUES (?, ?, ?, 4, 0)', (username, email, password))
+        db.commit()
+        db.close()
+        print("Sucessfully created database.")
 
 def get_db(): # Gets the database from the config array.
     if not hasattr(g, 'sqlite_db'):
@@ -58,10 +90,10 @@ def login():
     db = get_db().cursor()
     error = None
     if request.method == 'POST':
-        try: # TypeError means that the username doesn't exist. TODO: Fix this so that it doesn't require a try statement.
+        try: # None means that the username doesn't exist. TODO: Fix this so that it doesn't require a try statement.
             if request.form['username'] != db.execute('SELECT username FROM users WHERE username=?', (request.form['username'],)).fetchone()['username']:
                 error = 'Invalid username.'
-            elif request.form['password'] != db.execute('SELECT password FROM users WHERE username=?', (request.form['username'],)).fetchone()['password']:
+            elif request.form['password'] != decrypt(SECRET_KEY, db.execute('SELECT password FROM users WHERE username=?', (request.form['username'],)).fetchone()['password']):
                 error = 'Invalid password'
             else:
                 session['logged_in'] = True # Set the logged in cookie.
@@ -82,8 +114,8 @@ def register():
             error =  'Your password must be eight characters or longer.'
         else: # Attempt to insert the user
             try:
-                db.cursor().execute('INSERT INTO users (username, email, password, rank) VALUES (?, ?, ?, 1)', 
-                           [request.form['username'], request.form['email'], request.form['password']])
+                db.cursor().execute('INSERT INTO users (username, email, password, rank) VALUES (?, ?, ?, 1, 0)', 
+                           [request.form['username'], request.form['email'], encrypt(SECRET_KEY, request.form['password'])])
                 db.commit()
             except sqlite3.IntegrityError: # Username or email exists.
                 error = 'Username or email already taken.'
@@ -92,11 +124,11 @@ def register():
             return redirect(url_for('index'))
     return render_template('register.html', error=error)
     
-@app.route('/debug')
-def debug():
-    db = get_db().cursor()
-    value = db.execute('SELECT username FROM users WHERE username=?',("spideynn",)).fetchone()
-    return value[0]
+@app.route('/servers/<sid>', methods=['GET', 'POST'])
+def servers():
+    db = get_db()
+    error = None
+    return render_template('servers.html', error=error)
 
 @app.route('/logout')
 def logout():
